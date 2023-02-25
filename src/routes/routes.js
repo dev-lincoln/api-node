d43a8bdb663813ba5730d29e865ab0962ff21728
dev-lincoln <lincoln.dev@outlook.com>
2023-02-25 11:44:39 -0300

    update de rota get

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,7 +4,7 @@ import EstabController from "../controllers/EstabController";
 const router = Router();
 
 router.get("/", EstabController.listarEstabs);
-router.get("/:id", EstabController.listarEstab);
+router.get("/estab/:id", EstabController.listarEstab);
 router.post("/admin/add", EstabController.criarEstab);
 router.put("/admin/update/:id", EstabController.atualizarEstab);
 router.delete("/admin/delete/:id", EstabController.deletarEstab);
