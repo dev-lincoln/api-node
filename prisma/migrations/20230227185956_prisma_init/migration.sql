cb184ffd5c7aac0e963eafc47f00a0a9b4fb695e
dev-lincoln <lincoln.dev@outlook.com>
2023-02-27 16:04:13 -0300

    lutando

--- prisma/migrations/20230227185956_prisma_init/migration.sql
+++ prisma/migrations/20230227185956_prisma_init/migration.sql
@@ -3,9 +3,10 @@ CREATE TABLE "Estabelecimento" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "nome" TEXT NOT NULL,
     "endereco" TEXT NOT NULL,
+    "bairro" TEXT NOT NULL,
     "segmento" TEXT NOT NULL,
-    "telefone" INTEGER NOT NULL,
-    "whatsapp" INTEGER,
+    "telefone" TEXT NOT NULL,
+    "whatsapp" TEXT,
     "imagem" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
